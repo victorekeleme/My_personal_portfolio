c64a909490cae5c275aba4439808cb947e8fbaed
victorekeleme <ekelemevictor99@gmail.com>
2021-10-30 09:43:46 -0400

    project image

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -97,9 +97,11 @@ DATABASES = {
         'PASSWORD': 'paynen12',
         'HOST': 'localhost'
     }
-
-
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -151,7 +153,7 @@ django_heroku.settings(locals())
 
 
 MEDIA_URL = 'media/'
-MEIDA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEIDA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
