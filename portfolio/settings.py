92e804c7e1952f3720b3442bab464f3a2675f940
victorekeleme <ekelemevictor99@gmail.com>
2021-10-30 10:43:39 -0400

    project image

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,6 +15,8 @@ import os
 
 import django_heroku
 import  dj_database_url
+import cloudinary
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
