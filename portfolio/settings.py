81a02e06b27e571d827e921b80d06bc7ff1ed6e5
victorekeleme <ekelemevictor99@gmail.com>
2021-10-30 09:02:49 -0400

    pillow

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,13 +81,25 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolioDB',
+        'USER': 'postgres',
+        'PASSWORD': 'paynen12',
+        'HOST': 'localhost'
+    }
+
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
